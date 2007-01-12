425d94fdd3f07fd630fd5a4996fd454153c22124
Kristof Coomans <kristof.coomans@telenet.be>
2007-01-12 10:49:27 +0000

    - Corrected NuSOAP library version info - Mention applied patches to NuSOAP library

--- ezinfo.php
+++ ezinfo.php
@@ -10,9 +10,10 @@ class nusoapInfo
             'Author' => "Kristof Coomans",
             'License' => "GNU General Public License v2.0",
             'Includes the following third-party software' => array( 'Name' => 'NuSOAP',
-                                                                    'Version' => '0.7.1',
+                                                                    'Version' => '0.7.2 (cvs rev. 1.95)',
                                                                     'License' => 'GNU Lesser General Public License',
-                                                                    'More information' => 'http://sourceforge.net/projects/nusoap/'
+                                                                    'More information' => 'http://sourceforge.net/projects/nusoap/',
+                                                                    'Applied patches: ' => 'http://sourceforge.net/tracker/index.php?func=detail&aid=1008122&group_id=57663&atid=484965'
                                                                   )
         );
     }
